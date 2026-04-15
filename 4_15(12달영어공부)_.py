47fba151ebaad44d9c6f87147e04c19b94067c95
Seojooyeon-creat <79130628+Seojooyeon-creat@users.noreply.github.com>
2026-04-15 19:58:25 +0900

    modifying

--- 4_15(12달영어공부)_.py
+++ 4_15(12달영어공부)_.py
@@ -1,7 +1,6 @@
-kotoen = {"1": "January", "2": "February", "3": "March", "4": "April", "5": "May", "6": "June",
-          "7": "July", "8": "August", "9": "September", "10": "October", "11": "November", "12": "December"}
+kotoen = {"1": "January", "2": "February", "3": "March", "4": "April", "5": "May", "6": "June", "7": "July", "8": "August", "9": "September", "10": "October", "11": "November", "12": "December"}
 month = input("숫자 입력 (1~12):")
 if month in kotoen:
-    print(f"{month}월은 {kotoen[month]}입니다.")
+    print("해당 달:", kotoen[month])
 else:    
     print("1에서 12 사이의 숫자를 입력하세요.")
